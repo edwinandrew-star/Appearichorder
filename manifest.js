80146b4a6f44c8956d077d70737385011950c4f5
Masanja Edwin <eddandresw@gmail.com>
2026-08-10 23:10:27 -0700

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -2,22 +2,23 @@
   "name": "Appearich",
   "short_name": "Appearich",
   "start_url": "/",
+  "id": "/",
   "display": "standalone",
   "background_color": "#ffffff",
   "theme_color": "#ffffff",
   "orientation": "portrait",
   "icons": [
     {
-      "src": "images/appearich-logo1.png",
+      "src": "/images/appearich-logo1.png",
       "sizes": "192x192",
       "type": "image/png",
-      "purpose": "any maskable"
+      "purpose": "any"
     },
     {
-      "src": "images/appearich-logo.png",
+      "src": "/images/appearich-logo.png",
       "sizes": "512x512",
       "type": "image/png",
-      "purpose": "any maskable"
+      "purpose": "any"
     }
   ]
 }
