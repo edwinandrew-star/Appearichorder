9693a9d22f757b056996800b4d0c52de77a71eb6
Masanja Edwin <eddandresw@gmail.com>
2026-08-10 22:39:27 -0700

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -12,13 +12,19 @@
       "src": "images/appearich-logo1.png",
       "sizes": "192x192",
       "type": "image/png",
-      "purpose": "any maskable"
+      "purpose": "any"
     },
     {
       "src": "images/appearich-logo1.png",
       "sizes": "512x512",
       "type": "image/png",
-      "purpose": "any maskable"
+      "purpose": "any"
+    },
+    {
+      "src": "images/appearich-logo1.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "maskable"
     }
   ]
 }
